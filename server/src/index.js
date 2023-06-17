74392556eec4a69f748d62982069f7a68feefc3f
MitkoDG <retimid@abv.bg>
2023-06-17 21:42:51 +0300

    added UserSchema

--- server/src/index.js
+++ server/src/index.js
@@ -7,4 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect("mongodb+srv://appadmin:mernPassword123@recipe.s2rgwjc.mongodb.net/recipe?retryWrites=true&w=majority")
+
 app.listen(3001, () => console.log('listening on port 3001 -> http://localhost:3001'));
