4b1d2a8eb4ae73ebc2536aae6e79c091aff0661a
MitkoDG <retimid@abv.bg>
2023-06-17 22:02:45 +0300

    added register router

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { userRouter } from './routes/users.js';
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect("mongodb+srv://appadmin:mernPassword123@recipe.s2rgwjc.mongodb.net/recipe?retryWrites=true&w=majority")
 
