5b49eec75ec1edbd41f2773c0e0a34bc6115a6fc
MitkoDG <retimid@abv.bg>
2023-06-19 22:08:08 +0300

    config the Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from './components/Home';
+import { CreateRecipe } from './components/Create-recipe';
+import { SavedRecipes } from './components/Saved-recipe';
+import { Auth } from './components/Auth';
 
 function App() {
+
   return (
     <div>
-      <h1>App started</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-recipe' element={<CreateRecipe />} />
+          <Route path='/saved-recipe' element={<SavedRecipes />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
