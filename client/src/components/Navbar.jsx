1fa1cec8e646e9c140a741f1cb7db877c6ee1a77
MitkoDG <retimid@abv.bg>
2023-06-22 22:44:07 +0300

    redo the Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return(
-        <div className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/crete-recipe"> Create Recipe</Link>
-            <Link to="/saved-recipe"> Saved Recipe</Link>
-            <Link to="/auth"> Login/Register</Link>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <NavLink exact to="/" activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/create-recipe" activeClassName="active">
+        Create Recipe
+      </NavLink>
+      <NavLink to="/saved-recipe" activeClassName="active">
+        Saved Recipe
+      </NavLink>
+      <NavLink to="/auth" activeClassName="active">
+        Login/Register
+      </NavLink>
+    </div>
+  );
+};
