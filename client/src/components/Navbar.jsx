3dcdf836ec8e8730801c870edadc46b3f539b4ac
MitkoDG <retimid@abv.bg>
2023-06-25 00:03:17 +0300

    Auth page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { NavLink } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <NavLink exact to="/" activeClassName="active">
+      <NavLink exact="true" to="/" activeclassname="active">
         Home
       </NavLink>
-      <NavLink to="/create-recipe" activeClassName="active">
+      <NavLink to="/create-recipe" activeclassname="active">
         Create Recipe
       </NavLink>
-      <NavLink to="/saved-recipe" activeClassName="active">
+      <NavLink to="/saved-recipe" activeclassname="active">
         Saved Recipe
       </NavLink>
-      <NavLink to="/auth" activeClassName="active">
+      <NavLink to="/auth" activeclassname="active">
         Login/Register
       </NavLink>
     </div>
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,7 +1,76 @@
+import { useState } from "react";
+
 export const Auth = () => {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="auth">
+      <h2>It sucks</h2>
+      <Login />
+      <Regster />
     </div>
-  )
+  );
+};
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Login"
+    />
+  );
+};
+
+const Regster = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Regster"
+    />
+  );
+};
+
+const Form = ({ username, setUsername, password, setPassword, label }) => {
+  return (
+    <div className="auth-container">
+      <form>
+        <h2>{label}</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">{label}</button>
+      </form>
+    </div>
+  );
 };
