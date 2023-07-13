3e7c762d62bddea5209f7148672c9cc885bf332a
MitkoDG <retimid@abv.bg>
2023-07-13 23:05:27 +0300

    added form for create-recipe

--- client/src/pages/Create-recipe.jsx
+++ client/src/pages/Create-recipe.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+
 export const CreateRecipe = () => {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
   return (
-    <div>
-      <h1>Create recipe</h1>
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form action="POST">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="ingredients">Ingredients</label>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="imageUrl">ImageUrl</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+        />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+      </form>
     </div>
-  )
+  );
 };
